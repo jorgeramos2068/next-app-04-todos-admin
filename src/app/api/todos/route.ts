630797152ada063de95851ff8c9d38512a2ec3d1
Jorge Ramos <jorgeramos2068@gmail.com>
2024-07-11 09:16:33 -0600

    feat: add pagination to the list of todos

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -2,8 +2,21 @@ import { NextResponse } from 'next/server';
 
 import prisma from '@/lib/prisma';
 
-export async function GET() {
-  const todos = await prisma.todo.findMany();
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const take = searchParams.get('take') ?? '10';
+  const skip = searchParams.get('skip') ?? '0';
+  const parsedTake = parseInt(take);
+  const parsedSkip = parseInt(skip);
+
+  if (isNaN(parsedTake) || isNaN(parsedSkip)) {
+    return NextResponse.json({ message: 'The take and skip parameters must be numbers' }, { status: 400 });
+  }
+
+  const todos = await prisma.todo.findMany({
+    take: parsedTake,
+    skip: parsedSkip,
+  });
 
   return NextResponse.json(todos);
 }
