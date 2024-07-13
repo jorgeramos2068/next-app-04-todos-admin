5e4aa40b59500b1018f7bbef55032aac3ccb326b
Jorge Ramos <jorgeramos2068@gmail.com>
2024-07-13 13:36:22 -0600

    feat: add endpoint to update a todo record

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 
 import prisma from '@/lib/prisma';
+import { putTodosSchema } from '@/schemas';
 
 interface Context {
   params: Params;
@@ -25,4 +26,31 @@ export async function GET(_request: Request, context: Context) {
   }
 
   return NextResponse.json(todo);
+}
+
+export async function PUT(request: Request, context: Context) {
+  const { params } = context;
+  const { id } = params;
+
+  const todo = await prisma.todo.findUnique({
+    where: {
+      id: id,
+    },
+  });
+
+  if (!todo) {
+    NextResponse.json({ message: 'Record not found' }, { status: 404 });
+  }
+
+  try {
+    const body = await putTodosSchema.validate(await request.json());
+    const newData = {
+      description: body.description,
+      complete: body.complete,
+    };
+    const updatedTodo = await prisma.todo.update({ where: { id }, data: newData });
+    return NextResponse.json(updatedTodo);
+  } catch (error) {
+    return NextResponse.json(error, { status: 400 });
+  }
 }
--- src/schemas/todos.ts
+++ src/schemas/todos.ts
@@ -4,3 +4,8 @@ export const postTodosSchema = yup.object({
   description: yup.string().required(),
   complete: yup.boolean().optional().default(false),
 });
+
+export const putTodosSchema = yup.object({
+  description: yup.string().optional(),
+  complete: yup.boolean().optional(),
+});
